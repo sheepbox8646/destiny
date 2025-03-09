912cad9a67fe7d626772659bc8df5ae8cce67a70
Acbox <me@acbox.lol>
2025-03-10 03:34:28 +0800

    chore: callback

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,3 @@
 export const config = {
-  baseUrl: process.env.NODE_ENV === 'development' 
-    ? 'http://localhost:3000' 
-    : 'https://sticky-with.netlify.app'
+  baseUrl: process.env.CALLBACK_URL
 } 
