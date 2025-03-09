83a99aaa88ce130037b0d38ce91614597634fd42
Acbox <me@acbox.lol>
2025-03-10 03:25:07 +0800

    chore: unable type checks

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // !! 警告 !!
+    // 在生产环境中不建议禁用类型检查
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // 同时也禁用 ESLint
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
